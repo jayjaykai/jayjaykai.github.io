6a98c0ee7b0fa70207f5daab41266e56b5fde29b
jayjaykai <94094957+jayjaykai@users.noreply.github.com>
2024-06-11 16:52:37 +0800

    Add files via upload

---
+++ taipei-day-trip/static/booking_script.js
@@ -0,0 +1,68 @@
+// JavaScript 檢查區域是否為空
+document.addEventListener("DOMContentLoaded", function() {
+    // 檢查 token 並調用 getUserData
+    let token = localStorage .getItem('token');
+    if (token) {
+        getUserData();
+    } else {
+        alert('請先登入會員帳戶');
+        window.location.href = `/`;
+    }
+
+    // var personInfo = document.querySelector(".personInfo").innerHTML.trim();
+    // var creditCardInfo = document.querySelector(".creditCardInfo").innerHTML.trim();
+    // var confirm = document.querySelector(".confirm").innerHTML.trim();
+
+    // if (!personInfo && !creditCardInfo && !confirm) {
+    //     document.querySelector(".footer").classList.add("full-height-footer");
+    //     document.querySelector(".hr1").style.display = 'none';
+    //     document.querySelector(".hr2").style.display = 'none';
+    //     document.querySelector(".hr3").style.display = 'none';
+    // }
+});
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     let token = localStorage .getItem('token');
+//     if(token){
+//         getUserData()
+//     }
+// });
+
+async function getUserData() { 
+    try {
+        let token = localStorage .getItem('token');
+        if(!token){
+            alert('請先登入會員帳戶');
+            return;
+        }
+        let response = await fetch('http://127.0.0.1:8000/api/user/auth', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        
+        if (response.status === 403) {
+            alert('無效的憑證，請重新登入');
+            window.location.href = `/`;
+            return;
+        }
+
+        let result = await response.json();
+        console.log(result);
+        if (!response.ok) {
+            console.error('HTTP error', response.status);
+            alert(result.message);
+            window.location.href = `/`;
+        }
+
+        console.log(result.data);
+        let username = document.getElementById('username');
+        username.textContent = result.data.name;
+    } 
+    catch (error) {
+        console.error('Error:', error);
+    }
+}
--- taipei-day-trip/static/dialog_script.js
+++ taipei-day-trip/static/dialog_script.js
@@ -67,6 +67,28 @@ document.getElementById('loginButton').addEventListener('click', function() {
     }
 });
 
+async function bookEvent() {
+    let token = localStorage .getItem('token');
+    if(!token){
+        alert('請先登入會員帳戶');
+        return;
+    }
+    let response = await fetch('http://127.0.0.1:8000/api/user/auth', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    
+    if (response.status === 403) {
+        alert('無效的憑證，請重新登入');
+        window.location.href = `/`;
+        return;
+    }
+    window.location.href = `/booking`;
+}
+
 async function login() { 
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
@@ -95,9 +117,54 @@ async function login() {
             alert(result.message);
             return;
         }
-        let token = result.token;
-        console.log(token);
+        localStorage .setItem('token', result.token);
+
+        document.getElementById('email').value = '';
+        document.getElementById('password').value = '';
         document.getElementById('loginModal').close();
+    } 
+    catch (error) {
+        console.error('Error:', error);
+        alert('伺服器內部錯誤，請聯絡系統管理員');
+    }
+}
+
+async function signon() { 
+    let name = document.getElementById('enroll_name').value;
+    let email = document.getElementById('enroll_email').value;
+    let password = document.getElementById('enroll_password').value;
+
+    if (!email || !password || !name) {
+        alert('請輸入姓名、電子信箱和密碼');
+        return;
+    }
+
+    let userIfo = {
+        name: name,
+        email: email,
+        password: password
+    };
+
+    try {
+        let response = await fetch('http://127.0.0.1:8000/api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userIfo)
+        });
+        let result = await response.json();
+        if (!response.ok) {
+            console.error('HTTP error', response.status);
+            alert(result.message);
+            return;
+        }
+        console.log( result.ok);
+
+        document.getElementById('enroll_name').value = '';
+        document.getElementById('enroll_email').value = '';
+        document.getElementById('enroll_password').value = '';
+        document.getElementById('signonModal').close();
     } 
     catch (error) {
         console.error('Error:', error);
